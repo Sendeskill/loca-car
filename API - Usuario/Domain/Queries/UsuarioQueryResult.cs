f80eeaf03079eaf7273552cb74a232ffec1a672a
Lucas Tondo Sendeski <lucastsendeski@hotmail.com>
2022-07-19 18:28:18 -0300

    Update UsuarioQueryResult.cs

--- API - Usuario/Domain/Queries/UsuarioQueryResult.cs
+++ API - Usuario/Domain/Queries/UsuarioQueryResult.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public int Tipo { get; set; }
         public string Email { get; set; }
         public string Cpf { get; set; }
         public string Logradouro { get; set; }
